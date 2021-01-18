994719232b44c34af4793ce279e35145972b7c82
Tayyab Usman <tusman@itstrategists.com>
2021-01-19 04:56:39 +0500

    cleanup

--- LookerToSigmaConverter/models/presist_for.sql
+++
@@ -1,6 +0,0 @@
-
-        {{ config(materialized = "table") }}
-
-        {{ config(schema = "SIGMA_ITS_SIG") }}
-
-        SELECT CURRENT_TIMESTAMP, '' AS MODEL, '' AS SCHEMA WHERE 1 != 0
--- LookerToSigmaConverter/models/rp1.sql
+++ LookerToSigmaConverter/models/rp1.sql
@@ -28,65 +28,7 @@
 
         {%- set sourceSQL -%}
 
-        SELECT
-            ORDER_ID,
-            NEXT_ORDER_ID,
-            (CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN number_subsequent_orders >= 1 AND number_subsequent_orders <= 10 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 20 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 30 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 30 THEN TRUE 
-            ELSE FALSE 
-        END) AS LESS_THAN_40,
-(CASE 
-            WHEN next_order_id > 0 THEN TRUE 
-            ELSE FALSE 
-        END) AS HAS_SUBSEQUENT_ORDER,
-(CASE 
-            WHEN number_subsequent_orders >= 1 AND number_subsequent_orders <= 10 THEN TRUE 
-            ELSE FALSE 
-        END) AS LESS_THAN_10,
-(CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN number_subsequent_orders >= 1 AND number_subsequent_orders <= 10 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 20 THEN TRUE 
-            ELSE FALSE 
-        END) AS LESS_THAN_20,
-(CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN NOT 
-        (CASE 
-            WHEN number_subsequent_orders >= 1 AND number_subsequent_orders <= 10 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 20 THEN TRUE 
-            ELSE FALSE 
-        END) AND number_subsequent_orders <= 30 THEN TRUE 
-            ELSE FALSE 
-        END) AS LESS_THAN_30,
-        NUMBER_SUBSEQUENT_ORDERS,
-        CURRENT_TIMESTAMP AS LOAD_TIME
-            FROM (SELECT
-        order_items.order_id
-        , COUNT(DISTINCT repeat_order_items.id) AS number_subsequent_orders
-        , MIN(repeat_order_items.created_at) AS next_order_date
-        , MIN(repeat_order_items.order_id) AS next_order_id
-      FROM order_items
-      LEFT JOIN order_items repeat_order_items
-        ON order_items.user_id = repeat_order_items.user_id
-        AND order_items.created_at < repeat_order_items.created_at
-      GROUP BY 1)
+        SELECT * FROM {{ref('rp')}}
 
         {%- endset -%}
 
